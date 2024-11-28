37460185a1ab2f31f5e0f12657f6596e6659fc02
Pont-SimoSiren <sirensimo7@gmail.com>
2024-11-28 11:57:41 +0200

    Add project files.

---
+++ NumeroarvausPeli/Program.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Muuttujien esittely
+int arvaus;
+int arvottuLuku;
+bool jatketaanko = true;
+
+
+// Lista jonne arvaukset tallennetaan
+List<int> arvaukset = new List<int>();
+
+// Lukun arpominen
+Random rnd = new Random();
+arvottuLuku = rnd.Next(1, 10);
+
+while (jatketaanko == true)
+{
+    Console.WriteLine("Arvaa luku väliltä 1-10");
+
+    arvaus = int.Parse(Console.ReadLine()); // Otetaan käyttäjän syöte eli arvaus
+    arvaukset.Add(arvaus); // Lisätään arvaus listalle
+
+
+    if (arvaus > arvottuLuku)
+    {
+        Console.WriteLine("Arvottu luku on pienempi.");
+    }
+    else if (arvaus < arvottuLuku)
+    {
+        Console.WriteLine("Arvottu luku on suurempi.");
+    }
+    else
+    {
+        jatketaanko = false; // Tämänkertainen peli päättyy
+
+        Console.WriteLine("Arvasit oikein! Onneksi olkoon! Oikea luku oli " + arvottuLuku);
+
+        Console.WriteLine("Käytit yhteensä " + arvaukset.Count + " arvausta.");
+
+        Console.WriteLine("Alla näet kaikki antamasi arvaukset:");
+
+        // For silmukka eli for loop käy läpi kaikki arvaukset listalta
+        for (int i = 0; i < arvaukset.Count; i++)
+        {
+            Console.WriteLine(i + 1 + ". arvauksesi oli: " + arvaukset[i]);
+        }
+
+
+
+    }
+}
+
